16a76ff3f2ca8311a68030fbae89292f74815767
Andrew <whitedrew538@gmail.com>
2021-11-29 23:08:38 +0500

    tab

--- js/script.js
+++ js/script.js
@@ -3,5 +3,27 @@ const slider = tns({
     items: 1,
     slideBy: 'page',
     autoplay: false,
-    autoWidth: true
+    controls: false,
+    nav: false,
+    /* responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+        },
+        700: {
+            gutter: 30
+        },
+        900: {
+            items: 3
+        }
+    } */
 });
+
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+};
