7c2927451074bcf7c8697076294cf9cb00742490
Andrew <whitedrew538@gmail.com>
2021-12-05 20:15:37 +0500

    map

--- js/script.js
+++ js/script.js
@@ -4,20 +4,7 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false,
-    /* responsive: {
-        640: {
-            edgePadding: 20,
-            gutter: 20,
-            items: 2
-        },
-        700: {
-            gutter: 30
-        },
-        900: {
-            items: 3
-        }
-    } */
+    nav: false
 });
 
 document.querySelector('.prev').onclick = function () {
